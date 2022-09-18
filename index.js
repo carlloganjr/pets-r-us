0b25b9afafedb0ac0d264be662dd088a7d418f5d
Carl Logan <clogan@my365.bellevue.edu>
2022-09-18 00:00:08 -0600

    new folder - models, new file - register.html, modified the other views, index.js, main.css, and package.json

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ const ejs = require("ejs");
 const morgan = require("morgan");
 const boarding = require("./boarding.js");
 const training = require("./training.js");
+const Customers = require("./models/customers");
+const { default: mongoose } = require("mongoose");
 
+// set the address for the database
+const MONGO_CONNECT = "mongodb+srv://web340_admin:2NK75jyQAAuR1ejn@bellevueuniversity.dbdc0d0.mongodb.net/web340DB";
 // set the port on the Node global environment variable
 const PORT = process.env.PORT || 3000;
 // set the path for reference in the express static middleware
@@ -39,13 +43,21 @@ app.set("view engine", "html");
 // mainly used to ensure internal routing finds and can use the correct files
 app.use(express.static(mainPath));
 
+// make it easier to capture the values from the input on the registration page
+app.use(express.urlencoded({ extended: true })); 
+
+// set the format for the customer documents
+app.use(express.json()); 
+
 // place possible routes into an object for reference
 const routes = {
     "/": "index",
     "/index.html": "index",
     "/index": "index",
     "/grooming": "grooming",
-    "/grooming.html": "grooming"
+    "/grooming.html": "grooming",
+    "/register": "register",
+    "/register.html": "register"
 };
 
 // iterate over the routes object to find the requested route
@@ -56,12 +68,14 @@ for(let [key, value] of Object.entries(routes)) {
     });
 };
 
+// get the boarding pages when requested
 app.get("/boarding", (req, res) => {
     res.render("boarding", {
         boardingInformation: boarding.boardingInfo
     });
 });
 
+// get the training pages when requested
 app.get("/training", (req, res) => {
     res.render("training", {
         puppy: training.puppy,
@@ -70,9 +84,34 @@ app.get("/training", (req, res) => {
     });
 });
 
+// when the registration posts the request, add it to the database
+app.post("/register", (req, res) => {
+    const customers = new Customers({
+        customerId: req.body.customerId,
+        email: req.body.email
+    });
+
+    // check for errors loading to the database
+    // then redirect to the home page
+    Customers.create(customers, (err, customers) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+
 // handle a 404 error request
 app.use((req, res) => {
     res.status(404).render("404");
+});
+
+// connect to the database
+mongoose.connect(MONGO_CONNECT).then(() => {
+    console.log("MongoDB successfully connected.");
+}).catch((err) => {
+    console.log("MongoDB Error: " + err);
 });
 
 // listen for a connection on the specified port
